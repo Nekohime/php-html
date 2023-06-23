81cd0f8c02c1e5afa136414fbe6a5225ea606bb8
Nekohime <Nekohime@users.noreply.github.com>
2023-06-23 10:22:08 -0400

    First commit.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# php-html
+# \nekohime\HTML\HTML
+
+Better documentation and unit tests will be added soon:tm:.
+How to use:
+
+```php
+use \nekohime\HTML\HTML;
+HTML::link("https://münchen.de", "münchen.de", ['alt' => 'münchen.de!'], ['nl' => true, 'br' => true, 'external' => true]);
+```
+
+Add to composer.json:
+
+```json
+
+"repositories": [
+  {
+    "type": "git",
+    "url": "https://github.com/nekohime/php-html.git"
+  }
+],
+
+...
+
+"require": {
+  "nekohime/php-html": "dev-main"
+}
+
+```
---
+++ src/HTML.php
@@ -0,0 +1,65 @@
+<?php
+namespace nekohime\HTML;
+
+class HTML {
+
+	/*
+	* @param string $url RFC1738 compliant URL.
+	* @param string $text Link text to be shown for the clickable URL.
+	* @param array $attributes Any extra HTML attributes to add to the <a> tag.
+	* @param array $options Any extra options to be handled at our discretion.
+	*/
+	public static function link($url, $text, $attributes = array(), $options = array()) {
+		$url = htmlspecialchars($url);
+		$text = htmlspecialchars($text);
+
+		$output = '<a href="'.$url.'"';
+		if ($attributes) {
+			foreach ($attributes as $name => $value) {
+				$name = htmlspecialchars($name);
+				$value = htmlspecialchars($value);
+
+				if (is_bool($value)) {
+					if ($value) $output .= $name . ' ';
+				} else {
+					$output .= sprintf(' %s="%s"', $name, $value);
+				}
+			}
+		}
+
+		if (isset($options['external']) && $options['external'] === true) {
+			$output .= sprintf(' rel="nofollow noopener noreferrer"');
+		}
+
+		$br = (isset($options['br']) && $options['br'] === true) ? "<br>": "";
+		$nl = (isset($options['nl']) && $options['nl'] === true) ? "\n": "";
+
+		$output .= '>'.$text.'</a>' . $br . $nl;
+		echo $output;
+	}
+
+	// TODO: fix
+	// This alt vs title shit is exhausting.
+	// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img
+	public static function image($url, $alt, $attributes = array()) {
+		$url = filter_var($url, FILTER_SANITIZE_URL);
+		$alt = htmlspecialchars($alt);
+
+		$output = '<img src="'.$url.'" alt="'.$alt.'"';
+
+		foreach ($attributes as $name => $value) {
+			$name = htmlspecialchars($name);
+			$value = htmlspecialchars($value);
+
+			if (is_bool($value)) {
+				if ($value) $output .= $name . ' ';
+			} else {
+				$output .= sprintf(' %s="%s"', $name, $value);
+			}
+		}
+
+		$output .= '>';
+		echo $output;
+
+	}
+}
